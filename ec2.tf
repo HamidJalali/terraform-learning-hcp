f21546766b7c745190e714ed7391a3e878f119b5
HJ <36888678+HamidJalali@users.noreply.github.com>
2026-05-29 07:11:06 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-#resource "aws_instance" "myec2" {
-#    ami = "ami-05852c5f195d545ea"
-#    instance_type = "t3.micro"
-#}
+resource "aws_instance" "myec2" {
+    ami = "ami-05852c5f195d545ea"
+    instance_type = "t3.micro"
+}
